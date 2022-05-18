3031f88edcdbf82fba8395b1457f1c01159c7c02
Paul Melton <melton.paul@hotmail.com>
2022-05-18 16:35:21 -0500

    Built header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import heroImg from "./images/heroimg.jpg"
 export default function App() {
   return (
     <div className="container">
-      <header>
-        <img className="hero-img" src={heroImg} />
-        <h1>Find Your Film</h1>
-        <h2>My Watchlist</h2>
+      <header className="header">
+          <img className="hero-img" src={heroImg} />
+          <div className="hero-title">
+            <h1>Find Your Film</h1>
+            <h2 className="hero__watchlist">My Watchlist</h2>
+          </div>
       </header>
+      <main>
+        
+      </main>
     </div>
   )
 }
