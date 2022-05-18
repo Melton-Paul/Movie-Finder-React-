15ef7ec508ef6f2b22d44d77fe66106ea372b08b
Paul Melton <melton.paul@hotmail.com>
2022-05-18 16:13:36 -0500

    React Template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import heroImg from "./images/heroimg.jpg"
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <img className="hero-img" src={heroImg} />
+        <h1>Find Your Film</h1>
+        <h2>My Watchlist</h2>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+
