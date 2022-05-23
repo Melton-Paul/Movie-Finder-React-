73078531caacad1accb59347f122875d76051c95
Paul Melton <melton.paul@hotmail.com>
2022-05-23 16:54:52 -0500

    Memory w/ manual search button

--- src/App.js
+++ src/App.js
@@ -5,78 +5,78 @@ import Watchlist from "./components/Watchlist"
 import FindFilms from "./components/FindFilms"
 
 export default function App() {
-  const [loading, setLoading] = React.useState(false)
-  const [movies, setMovies] = React.useState([])
-  const [searchValue, setSearchValue] = React.useState("")
-  const [searchMemory, setSearchMemory] = React.useState("")
-  const [watchlistStorage, setWatchlistStorage] = React.useState(JSON.parse(window.localStorage.getItem("watchlist")))
-  const [movieHtml, setMovieHtml] = React.useState("")
+  // const [loading, setLoading] = React.useState(false)
+  // const [movies, setMovies] = React.useState([])
+  // const [searchValue, setSearchValue] = React.useState("")
+  // const [searchMemory, setSearchMemory] = React.useState("")
+  const [watchlistStorage, setWatchlistStorage] = React.useState(JSON.parse(window.localStorage.getItem("watchlist")) || [])
+  // const [movieHtml, setMovieHtml] = React.useState("")
   const [watchlistPage, setWatchlistPage ] = React.useState(JSON.parse(window.localStorage.getItem("page")))
   let typingTimer
   console.log(watchlistStorage)
   console.log(localStorage)
-  React.useEffect(()=>{
-    if(!searchValue){
-      return 
-    }
-      fetch(`https://www.omdbapi.com/?apikey=9980ac75&s=${searchValue}`)
-        .then(res => res.json())
-        .then(data => {
-          let movieList = []
-          data.Search.forEach(movie=>{
-            fetch(`https://www.omdbapi.com/?apikey=9980ac75&t=${movie.Title}`)
-            .then(res => res.json())
-            .then(data => {
-              movieList.push(data)
-              console.log(data)
-            })
-          })
-          console.log(movieList)
-          setMovies(movieList)
-          getHTML()
-          console.log(movies)
-        })
-    }, [searchMemory])
+  // React.useEffect(()=>{
+  //   if(!searchValue){
+  //     return 
+  //   }
+  //     fetch(`https://www.omdbapi.com/?apikey=9980ac75&s=${searchValue}`)
+  //       .then(res => res.json())
+  //       .then(data => {
+  //         let movieList = []
+  //         data.Search.forEach(movie=>{
+  //           fetch(`https://www.omdbapi.com/?apikey=9980ac75&t=${movie.Title}`)
+  //           .then(res => res.json())
+  //           .then(data => {
+  //             movieList.push(data)
+  //             console.log(data)
+  //           })
+  //         })
+  //         console.log(movieList)
+  //         setMovies(movieList)
+  //         getHTML()
+  //         console.log(movies)
+  //       })
+  //   }, [searchMemory])
     
     
-  function getHTML(){
-    const movieArr = movies.map(movie => {
-      return <MovieCard props={{...movie}} addStorage={addStorage} watchlistStorage={watchlistStorage} removeStorage={removeStorage} />})
-      console.log(movieArr)
-    setMovieHtml(movieArr)
-    }
+  // function getHTML(){
+  //   const movieArr = movies.map(movie => {
+  //     return <MovieCard props={{...movie}} addStorage={addStorage} watchlistStorage={watchlistStorage} removeStorage={removeStorage} />})
+  //     console.log(movieArr)
+  //   setMovieHtml(movieArr)
+  //   }
 
   
-  function handleChange(event){
-    clearTimeout(typingTimer)
-    setLoading(true)
-    setSearchValue(event.target.value)
-    typingTimer = setTimeout(()=>{ 
-      setSearchMemory(searchValue)
-      setLoading(false)
-    }, 2500)
-  }
+  // function handleChange(event){
+  //   clearTimeout(typingTimer)
+  //   setLoading(true)
+  //   setSearchValue(event.target.value)
+  //   typingTimer = setTimeout(()=>{ 
+  //     setSearchMemory(searchValue)
+  //     setLoading(false)
+  //   }, 2500)
+  // }
 
-  function html(){ 
-    if(searchValue){
-      if(loading){
-        return ( 
-          <div id="noData">
-            <img className="loading" src={loadingImg} />
-            <p>Fetching Movies</p>
-          </div> 
-        ) 
-      } else {
-        return movieHtml
-      }
-      } else {
-        return (
-          <div id="noData">
-            <i className="fa fa-film fa-6x"></i>
-            <p>Start Exploring</p>
-          </div>
-        )
-  }}
+  // function html(){ 
+  //   if(searchValue){
+  //     if(loading){
+  //       return ( 
+  //         <div id="noData">
+  //           <img className="loading" src={loadingImg} />
+  //           <p>Fetching Movies</p>
+  //         </div> 
+  //       ) 
+  //     } else {
+  //       return movieHtml
+  //     }
+  //     } else {
+  //       return (
+  //         <div id="noData">
+  //           <i className="fa fa-film fa-6x"></i>
+  //           <p>Start Exploring</p>
+  //         </div>
+  //       )
+  // }}
 
   function addStorage(id){
     setWatchlistStorage(prev => {
@@ -101,10 +101,12 @@ export default function App() {
   return (
     <div>
       {watchlistPage ? 
-      <Watchlist setWatchlistPage={setWatchlistPage} watchlistStorage={watchlistStorage}  removeStorage={removeStorage} MovieCard={MovieCard} getHTML={getHTML}/> : 
-      <FindFilms handleChange={handleChange} searchValue={searchValue} setWatchlistPage={setWatchlistPage} html={html} />}
+      <Watchlist setWatchlistPage={setWatchlistPage} watchlistStorage={watchlistStorage}  removeStorage={removeStorage} MovieCard={MovieCard} /> : 
+      <FindFilms loadingImg={loadingImg} setWatchlistPage={setWatchlistPage} watchlistStorage={watchlistStorage} addStorage={addStorage} removeStorage={removeStorage}  />}
     </div>
   )
 }
+
+
 
 
--- src/components/FindFilms.js
+++ src/components/FindFilms.js
@@ -1,8 +1,84 @@
 import heroImg from "../images/heroimg.jpg"
 import React from "react"
+import MovieCard from "./MovieCard"
 
 
 export default function FindFilms(props){
+    const [searchValue, setSearchValue] = React.useState("")
+    const [movies, setMovies] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [movieHtml, setMovieHtml] = React.useState("")
+    let typingTimer
+
+
+
+
+
+
+
+    React.useEffect(()=>{
+
+        if(!searchValue){
+          return 
+        }
+          fetch(`https://www.omdbapi.com/?apikey=9980ac75&s=${searchValue}`)
+            .then(res => res.json())
+            .then(data => {
+              let movieList = []
+              data.Search.forEach(movie=>{
+                fetch(`https://www.omdbapi.com/?apikey=9980ac75&t=${movie.Title}`)
+                .then(res => res.json())
+                .then(data => {
+                  movieList.push(data)
+                  console.log(data)
+                  setMovies(movieList)
+                  getHTML()
+                })
+              })
+              console.log(movieList)
+              console.log(movies)
+            })
+        }, [loading])
+        
+        function getHTML(){
+            const movieArr = movies.map(movie => {
+              return <MovieCard props={{...movie}} addStorage={props.addStorage} watchlistStorage={props.watchlistStorage} removeStorage={props.removeStorage} />})
+              console.log(movieArr)
+            setMovieHtml(movieArr)
+            }
+
+            function handleChange(){
+                const search = document.getElementById("searchBar")
+                console.log(search.value)
+                clearTimeout(typingTimer)
+                setLoading(true)
+                setSearchValue(search.value)
+                typingTimer = setTimeout(()=>{ 
+                  setLoading(false)
+                }, 2500)
+              }
+              function html(){ 
+                if(searchValue){
+                  if(loading){
+                    return ( 
+                      <div id="noData">
+                        <img className="loading" src={props.loadingImg} />
+                        <p>Fetching Movies</p>
+                      </div> 
+                    ) 
+                  } else {
+                    return movieHtml
+                  }
+                  } else {
+                    return (
+                      <div id="noData">
+                        <i className="fa fa-film fa-6x"></i>
+                        <p>Start Exploring</p>
+                      </div>
+                    )
+              }}
+
+        
 
     return (
         <div className="findfilms-page">
@@ -14,12 +90,12 @@ export default function FindFilms(props){
                 </div>
                 <div className="input-group">
                     <i className="fa fa-search"></i>
-                    <input type="text" id="searchBar" placeholder="What are you looking for?" onChange={props.handleChange} value={props.searchValue} autoFocus />
-                    <button className="btn" type="submit" id="searchBtn">Search</button>
+                    <input type="text" id="searchBar" placeholder="What are you looking for?" autoFocus />
+                    <button className="btn" type="submit" id="searchBtn" onClick={handleChange}>Search</button>
                 </div>
             </header>
             <main> 
-                {props.html()}
+                {html()}
             </main>
         </div>
     )
