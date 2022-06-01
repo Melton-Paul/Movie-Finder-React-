bc20b26bf1e907b7b8ba62764c1394ee96ba7b8d
Paul Melton <melton.paul@hotmail.com>
2022-06-01 16:00:12 -0500

    Installed Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./styles.css"
 import App from './App';
+import FindFilms from "./components/FindFilms"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App />}>
+                <Route path="" />
+            </Route>
+
+        </Routes>
+    </BrowserRouter>
+
+, document.getElementById("root"))
