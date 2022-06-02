a82005c68c8fa6d906b6317cd9fb0255866cbcc8
Paul Melton <melton.paul@hotmail.com>
2022-06-02 14:40:51 -0500

    URL param error handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Outlet, BrowserRouter, Routes, Route } from "react-router-dom"
+import {BrowserRouter, Routes, Route } from "react-router-dom"
 import FindFilms from "./components/FindFilms"
 import Watchlist from "./components/Watchlist"
 import usePageLogic from "./hooks/usePageLogic"
@@ -16,11 +16,14 @@ export default function App() {
       addStorage,
      } =  usePageLogic()
     
+     const findFilms = <FindFilms watchlistStorage={watchlistStorage} removeStorage={removeStorage} addStorage={addStorage} />
+
     return (
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<FindFilms watchlistStorage={watchlistStorage} removeStorage={removeStorage} addStorage={addStorage} />} />
+            <Route path="/" element={findFilms} />
             <Route path="/watchlist" element={<Watchlist watchlistStorage={watchlistStorage} removeStorage={removeStorage}  />} />
+            <Route path="*" element={findFilms} />
         </Routes>
       </BrowserRouter>
   )
